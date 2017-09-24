fe300c6dfbefeb13bf55324ee3d6d94a814222c3
SubhajitPoddar <subhajitp@SubhajitPoddars-MacBook-Air.local>
2017-09-25 01:50:08 +0530

    Committed

---
+++ Alomofire/ViewController.swift
@@ -0,0 +1,39 @@
+//
+//  ViewController.swift
+//  Alomofire
+//
+//  Created by SubhajitPoddar on 24/09/17.
+//  Copyright © 2017 SubhajitPoddar. All rights reserved.
+//
+
+import UIKit
+import Alamofire
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        Alamofire.request("https://httpbin.org/get").responseJSON { response in
+            print("Request: \(String(describing: response.request))")   // original url request
+            print("Response: \(String(describing: response.response))") // http url response
+            print("Result: \(response.result)")                         // response serialization result
+            
+            if let json = response.result.value {
+                print("JSON: \(json)") // serialized json response
+            }
+            
+            if let data = response.data, let utf8Text = String(data: data, encoding: .utf8) {
+                print("Data: \(utf8Text)") // original server data as UTF8 string
+            }
+        }
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
